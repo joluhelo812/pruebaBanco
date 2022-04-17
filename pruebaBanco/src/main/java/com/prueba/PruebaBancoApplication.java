2edb85bb6efa31312226794d500d8606b5c13c9e
joluhelo <jluis.henao@udea.edu.co>
2022-04-17 12:07:07 -0500

    Primer commit

---
+++ pruebaBanco/src/main/java/com/prueba/PruebaBancoApplication.java
@@ -0,0 +1,13 @@
+package com.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBancoApplication.class, args);
+	}
+
+}
---
+++ pruebaBanco/src/test/java/com/prueba/model/PruebaBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
